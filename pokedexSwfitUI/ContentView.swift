f0f7ef2dc05a80034decfdb8966d2e6ee1c9f9c3
@eko.prasetiyo <eko.prasetiyo@sent.tv>
2021-03-17 10:37:59 +0700

    Initial Commit

---
+++ pokedexSwfitUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pokedexSwfitUI
+//
+//  Created by Eko Prasetiyo on 17/03/21.
+//  Copyright © 2021 coba. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
