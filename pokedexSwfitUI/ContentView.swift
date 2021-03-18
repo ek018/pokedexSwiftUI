4d461b57ba63eac364fea1d5516e37aa2bf1565f
@eko.prasetiyo <eko.prasetiyo@sent.tv>
2021-03-18 09:31:17 +0700

    learning VStack, HStack, ZStack and list part 1 #1

--- pokedexSwfitUI/ContentView.swift
+++ pokedexSwfitUI/ContentView.swift
@@ -10,7 +10,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World! ")
+        NavigationView {
+            ZStack {
+                Image("bg")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                BodyView()
+                
+            }
+            
+        }
     }
 }
 
@@ -19,3 +28,75 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct BodyView: View {
+    var body: some View {
+        VStack{
+            LogoView()
+            FormView()
+        }
+        
+    }
+}
+
+struct LogoView: View {
+    var body: some View {
+        VStack{
+            Image("pokemon_logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding()
+                .frame(width: 200, height: 200, alignment: .center)
+        }
+    }
+    
+}
+
+struct FormView: View {
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var isValid: Bool = false
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8){
+            Text("Email")
+            TextField("Place your email here", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Text("Password")
+            SecureField("Place your password here", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            NavigationLink(destination: HomeView()){
+                Text("Sign in")
+                    .frame(minWidth: 0, maxWidth: .infinity,
+                           minHeight: 0, maxHeight: 48)
+                    .background(Color.yellow)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(8)
+                    .padding(.top, 32)
+            }
+            //            NavigationLink(destination: HomeView()){
+            //                Button(action: {print("GO Pokemon!!")}) {
+            //                    Text("Sign in")
+            //                        .frame(minWidth: 0, maxWidth: .infinity,
+            //                               minHeight: 0, maxHeight: 48)
+            //                    Spacer()
+            //                }.background(Color.yellow)
+            //                    .foregroundColor(Color.white)
+            //                    .cornerRadius(8)
+            //                    .padding(.top, 32)
+            //
+            //
+            //            }
+            
+            
+            
+            
+        }.padding(.horizontal, 24)
+    }
+}
+
+
+
+
+
